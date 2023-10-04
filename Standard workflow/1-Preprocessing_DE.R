2129fc2538c3622d520b6146f8a6fdb8b08b51c0
Katia <katiaotterstedt@gmail.com>
2023-10-04 09:11:19 -0300

    RScripts added

---
+++ Standard workflow/1-Preprocessing_DE.R
@@ -0,0 +1,150 @@
+## ----message=FALSE, warning = FALSE--------------
+library(RTCGA.clinical)
+library(TCGAbiolinks)
+library(tidyverse)
+library(edgeR)
+library(SummarizedExperiment)
+
+
+## ----message=FALSE, warning = FALSE--------------
+clinical_COAD <- GDCquery_clinic("TCGA-COAD")
+barcodes_to_download <- clinical_COAD$bcr_patient_barcode
+
+
+## ----message=FALSE, warning = FALSE, eval = FALSE----
+## query_TCGA <- GDCquery(project = "TCGA-COAD",
+##                        data.category = "Transcriptome Profiling",
+##                        data.type = "Gene Expression Quantification",
+##                        experimental.strategy = "RNA-Seq",
+##                        workflow.type = "STAR - Counts",
+##                        sample.type = "Primary Tumor",
+##                        barcode= barcodes_to_download,
+##                        access = "open")
+## 
+## GDCdownload(query_TCGA)
+## tcga_coad_SE <- GDCprepare(query_TCGA, summarizedExperiment = TRUE)
+## 
+## #Extract counts and gene metadata
+## counts_matrix <- assay(tcga_coad_SE)
+## gene_metadata <- as.data.frame(rowData(tcga_coad_SE))
+## row.names(counts_matrix) <- (gene_metadata$gene_name)
+## 
+
+
+## ------------------------------------------------
+# Filtra las filas basadas en los nombres de fila
+counts_matrix_filt_bef <- counts_matrix[!grepl("^MIR|^LINC|^Y_", rownames(counts_matrix)), ]
+
+#We proceed to check the dimensions of both matrix 
+dim(counts_matrix)
+dim(counts_matrix_filt_bef)
+
+
+
+## ----echo = FALSE, results='hide'----------------
+load("~/Bioinfo Tesina/Common data gefs&degs.RData") 
+
+
+
+## ----message=FALSE, warning = FALSE--------------
+source("tcga_replicateFilter.R")
+total_patients <- c(colnames(counts_matrix))
+filtered_patients <- tcga_replicateFilter(tsb = total_patients,
+                                          analyte_target = "RNA")
+counts_matrix <- counts_matrix[,filtered_patients]
+
+
+## ----message=FALSE, warning = FALSE--------------
+counts_matrix_normalised <- cpm(counts_matrix)
+
+
+## ----message=FALSE, warning = FALSE--------------
+counts_matrix_normalised <- as.data.frame(counts_matrix_normalised) 
+
+# We need the short version of the barcodes to merge expression and survival
+long_patient_names <- colnames(counts_matrix_normalised)
+
+shortened_patient_names <- substr(long_patient_names, 1, 12)
+
+# Converting from vector to df to be added as a row to our normalised counts matrix 
+shortened_patient_names <- as.data.frame(t(shortened_patient_names))
+
+colnames(shortened_patient_names) <- colnames(counts_matrix_normalised)
+
+# Adding short names as a row to normalised matrix 
+counts_matrix_normalised <- rbind(shortened_patient_names, counts_matrix_normalised)
+
+# Changing the name to make the merge easier
+row.names(counts_matrix_normalised)[1] <- "bcr_patient_barcode"
+
+# Searching the row number of our gene of interest
+which(row.names(counts_matrix_normalised) %in% c("RAC1"))
+
+table(is.na(counts_matrix_normalised[7245,])) 
+
+
+## ----message=FALSE, warning = FALSE--------------
+# Creating a column to represent presence/absence of event
+clinical_COAD$event <- ifelse(clinical_COAD$vital_status == "Alive",
+                                 0,
+                                 1)
+table(clinical_COAD$event) 
+
+#We create an "overal survival variable"
+clinical_COAD$overall_survival <- ifelse(clinical_COAD$vital_status == "Alive",
+                                         clinical_COAD$days_to_last_follow_up, #If alive => last follow up to OS
+                                         clinical_COAD$days_to_death) #If dead => death to OS
+
+
+
+## ----message=FALSE, warning = FALSE--------------
+#If still alive at 5 years, we consider 0, didn't present event
+clinical_COAD$event_5y <- ifelse(clinical_COAD$overall_survival >= 1825,
+                                 0, 
+                                 clinical_COAD$event)
+table(clinical_COAD$event_5y)
+
+# If time is longer than 5 years (1825 days) we asign 1825 days as time
+clinical_COAD$overall_survival_5y <- ifelse(clinical_COAD$overall_survival >= 1825,
+                                            1825,
+                                            clinical_COAD$overall_survival)
+
+
+## ----message=FALSE, warning = FALSE--------------
+
+which(colnames(clinical_COAD) %in% c("overall_survival_5y", "event_5y", "bcr_patient_barcode",
+                                     "overall_survival", "event"))
+
+COAD.surv <- clinical_COAD[,c(70,71,72,73,74)]
+table(is.na(COAD.surv$event_5y))
+COAD.surv <- COAD.surv %>%
+  filter(!is.na(event_5y))
+
+# Combining survival data with expression data of gene of interest
+COAD.surv <- merge(COAD.surv, t(counts_matrix_normalised[c(1,7245),]), by = "bcr_patient_barcode")
+# Converting the column to type integer 
+COAD.surv$RAC1 <- as.integer(COAD.surv$RAC1)
+table(is.na(COAD.surv$RAC1))
+
+
+## ----message=FALSE, warning = FALSE--------------
+boxplot(COAD.surv$RAC1, col = "palevioletred")
+
+# Get mean and standard deviation
+mean = mean(COAD.surv$RAC1)
+std = sd(COAD.surv$RAC1)
+
+# Get threshold values for outliers
+Tmin = mean-(3*std)
+Tmax = mean+(3*std)
+
+# Find outlier expression values
+outliers <- which(COAD.surv$RAC1 < Tmin | COAD.surv$RAC1>= Tmax)
+
+# Assing NA values to outliers
+COAD.surv$RAC1[outliers] <- NA
+
+
+## ------------------------------------------------
+COAD.surv$RAC1 <- as.integer(COAD.surv$RAC1)
+
---
+++ Standard workflow/2-DE_Analysis.R
@@ -0,0 +1,165 @@
+## ----echo = FALSE, results='hide'-------------------------------------------------------------------------
+load("~/Bioinfo Tesina/Common data gefs&degs.RData") 
+
+
+
+## ----message=FALSE, warning = FALSE-----------------------------------------------------------------------
+library(survminer)
+library(edgeR)
+library(EnhancedVolcano)
+library(RColorBrewer) # For a colourful plot
+library(ggrepel)
+
+
+## ---------------------------------------------------------------------------------------------------------
+COAD.cut <- surv_cutpoint(
+  COAD.surv,
+  time = "overall_survival",
+  event = "event",
+  variables = "RAC1")
+
+# Getting the value of cutpoint in CPM and statistic
+summary(COAD.cut)
+
+# Checking some statistics related to out gene of interest
+mean(COAD.surv$RAC1, na.rm = TRUE) 
+sd(COAD.surv$RAC1, na.rm = TRUE)
+median(COAD.surv$RAC1, na.rm = TRUE)
+
+
+# Plotting the cutpoint
+plot(COAD.cut, "RAC1", palette = "npg")
+
+# Extracting cutpoint
+cutpoint <- COAD.cut[["cutpoint"]]$cutpoint
+
+
+## ---------------------------------------------------------------------------------------------------------
+#RAC1 is row 7245
+groups <- ifelse(counts_matrix_normalised[7245, ] <= cutpoint, "low", "high")
+table(groups)
+
+
+## ---------------------------------------------------------------------------------------------------------
+# We first create a DGEList object
+y <- DGEList(counts=counts_matrix, group = groups)
+# Setting "low" expression level as control
+y$samples$group <- relevel(y$samples$group, ref="low")
+# Filter out lowly expressed genes
+keep <- filterByExpr(y) 
+summary(keep)
+y<- y[keep,,keep.lib.sizes=FALSE]
+
+
+## ----fig.width=50, fig.height=15, warning=FALSE, message=FALSE--------------------------------------------
+#Plotting data before normalisation
+nsamples <- ncol(y) #Number of samples
+col <- brewer.pal(nsamples, "Paired")
+lcpm <- cpm(y, log=TRUE)
+boxplot(lcpm, las=2, col=col, main="")
+title(main="A. Unnormalised data",ylab="Log-cpm")
+
+# Performs the trimmed mean of M-values (TMM) normalization calculating normalization factors
+y <- calcNormFactors(y, method = "TMM") 
+
+# Creating a design matrix 
+design <- model.matrix(~ 0 + y$samples$group)
+colnames(design) <- levels(y$samples$group)
+
+#Plotting data after normalisation 
+lcpm <- cpm(y, log=TRUE)
+boxplot(lcpm, las=2, col=col, main="")
+title(main="B. Normalised data",ylab="Log-cpm")
+
+# Calculates dispersion using classic mode
+y <- estimateDisp(y, design = design, robust = T) # We use robust to treat possible outliers
+
+# Performs an exact test for negative binomial distribution
+et <- exactTest(y) # Calculates p-values
+
+# Makes decisions about which genes show significant differences
+is.de <- decideTestsDGE(et) # Uses p-values
+summary(is.de)
+
+# Obtaining a summary of the most significant results in the analysis
+# n = Inf => obtaing all results from the analysis
+et_tags<-topTags(et, n = Inf)
+
+x<-as.data.frame(et_tags)
+
+
+# Classifying into upregulated/stable/downregulated using FDR < 0.05
+# logFC >= 1
+x$Expression = ifelse(x$FDR < 0.05 & abs(x$logFC) >= 1, 
+                      ifelse(x$logFC> 1 ,'Up','Down'),
+                      'Stable')
+table(x$Expression)
+
+# logFC >= 2
+x$Expression2 = ifelse(x$FDR < 0.05 & abs(x$logFC) >= 2, 
+                       ifelse(x$logFC> 2 ,'Up','Down'),
+                       'Stable')
+table(x$Expression2)
+
+# Obtaining names of up and down genes
+genes_up <- row.names(x[(x$Expression == "Up")&(x$PValue<0.05), ])
+genes_down<- row.names(x[(x$Expression == "Down")&(x$PValue<0.05), ])
+
+
+## ----fig.width=15, fig.height= 10-------------------------------------------------------------------------
+EnhancedVolcano(x,
+                lab = rownames(x),
+                x= "logFC",
+                y= "PValue",
+                title = "HIGH Rac vs. LOW Rac",
+                pCutoff = 10e-32,
+                FCcutoff = 1,
+                pointSize = 3.0,
+                labSize = 3.0,
+                xlim = c(-5,5),
+                # boxedLabels = TRUE,
+                col = c("azure4", "palegreen3", "skyblue3", "palevioletred"),
+                colAlpha = 0.5,
+                legendPosition = 'right')
+
+
+## ---------------------------------------------------------------------------------------------------------
+v <- voom(y, design, plot=TRUE) #Produces EList object
+
+fit <- lmFit(v, design)
+head(coef(fit))
+
+contr <- makeContrasts(high - low, levels = colnames(coef(fit)))
+contr
+
+tmp <- contrasts.fit(fit, contr)
+
+tmp <- eBayes(tmp) #Multiple testing errors => removes false positives 
+top.table <- topTable(tmp, sort.by = "P", n = Inf) #Extract all DEGs, sort by pvalue
+head(top.table, 20)
+
+#Get the number of DEGs
+length(which(top.table$adj.P.Val < 0.05))
+
+top.table$Expression = ifelse(top.table$adj.P.Val < 0.05 & abs(top.table$logFC) >= 1, 
+                            ifelse(top.table$logFC> 1 ,'Up','Down'),
+                            'Stable')
+table(top.table$Expression)
+
+
+## ----fig.width=15, fig.height= 10-------------------------------------------------------------------------
+EnhancedVolcano(top.table,
+                lab = top.table$ID,
+                x= "logFC",
+                y= "adj.P.Val",
+                title = "HIGH Rac vs. LOW Rac",
+                pCutoff = 10e-32,
+                FCcutoff = 1,
+                pointSize = 3.0,
+                labSize = 3.0,
+                xlim = c(-5,5),
+                # boxedLabels = TRUE,
+                col = c("azure4", "palegreen3", "skyblue3", "palevioletred"),
+                colAlpha = 0.5,
+                legendPosition = 'right')
+
